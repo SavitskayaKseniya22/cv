71c3cf069b947c291fac99b10f6bcd3c7fdbe666
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-01-16 13:57:48 +0200

    feat: add error component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BackButton from '@/components/backButton';
+
+function NotFound() {
+  return (
+    <>
+      <h1>Not Found</h1>
+      <BackButton />
+    </>
+  );
+}
+
+export default NotFound;
