ac4dcce7bce413ddba877a332be2e6a921bc0f72
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-04-03 23:34:08 +0300

    refactor: bio and add photo

--- src/app/about-me/components/bio.tsx
+++ src/app/about-me/components/bio.tsx
@@ -1,23 +1,65 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { ScreenSize } from '@/app/interfaces';
+
+const StyledBioContent = styled('div')`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  align-items: center;
+  flex-grow: 2;
+
+  @media ${ScreenSize.TABLET} {
+    gap: 2rem;
+    flex-direction: row;
+  }
+
+  img {
+    width: 150px;
+    height: auto;
+
+    @media ${ScreenSize.LAPTOPL} {
+      width: auto;
+    }
+  }
+`;
+
+const StyledBioImage = styled(Image)`
+  width: 150px;
+  height: auto;
+
+  @media ${ScreenSize.LAPTOPL} {
+    width: auto;
+  }
+`;
 
 function Bio() {
   return (
-    <div>
-      <h2>Bio</h2>
-      <h3>Born: November 8, 1993, Russia</h3>
-      <h3>Location: Sofia, Bulgaria</h3>
-      <p>
-        I am 30 years old. I am a versatile person. My hobbies are cinema,
-        handicrafts, video games, programming. I believe that a person needs to
-        constantly be in development, learning new things, improving acquired
-        skills. I have no commercial development experience, but I am able to
-        maintain interest for a long time, I can compensate for the gaps in
-        education with diligence and perseverance.
-      </p>
-      <h3>Languages: English - B2, Russian - native</h3>
-    </div>
+    <StyledBioContent>
+      <div>
+        <h3>Born: November 8, 1993, Russia</h3>
+        <h3>Location: Sofia, Bulgaria</h3>
+        <p>
+          I am 30 years old. I am a versatile person. My hobbies are cinema,
+          handicrafts, video games, programming. I believe that a person needs
+          to constantly be in development, learning new things, improving
+          acquired skills. I have no commercial development experience, but I am
+          able to maintain interest for a long time, I can compensate for the
+          gaps in education with diligence and perseverance.
+        </p>
+        <h3>Languages: English - B2, Russian - native</h3>
+      </div>
+      <StyledBioImage
+        src="/profile.jpg"
+        width={200}
+        height={230}
+        alt="profile"
+        priority
+      />
+    </StyledBioContent>
   );
 }
 
