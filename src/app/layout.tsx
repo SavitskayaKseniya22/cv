71d687b8f65b5168307f7235225cf92fde0624ff
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-04-03 23:32:11 +0300

    feat: add favicon and font options

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import type { Metadata } from 'next';
 import StyledComponentsRegistry from '@/components/registry';
 import { Jura } from 'next/font/google';
-
 import MainLayout from './components/main-layout';
 import 'normalize.css/normalize.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './globals.scss';
 import Status from './components/status';
 
-const inter = Jura({ subsets: ['latin'] });
+const inter = Jura({
+  subsets: ['latin'],
+  style: 'normal',
+  weight: ['400', '700'],
+});
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'CV',
   description: 'Ksenia Savitskaia"s portfolio',
+  icons: {
+    icon: './favicon.ico',
+  },
 };
 
 export default function RootLayout({
