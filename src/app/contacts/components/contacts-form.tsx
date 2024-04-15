0e5ace736131c57c0f76281a05b7750484ec8e2e
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-04-15 21:59:57 +0300

    feat: add email cending to form in contacts

--- src/app/contacts/components/contacts-form.tsx
+++ src/app/contacts/components/contacts-form.tsx
@@ -2,26 +2,41 @@
 
 'use client';
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import emailjs from '@emailjs/browser';
 
-export const StyledInput = styled('input')`
+const emailServiceId = 'service_h0yuidd';
+const emailTemplateId = 'template_8fl60sd';
+const emailPublickKey = 'SDGBlEDoTs9cNxtLw';
+
+export const StyleForInput = css`
   padding: 0.5rem;
   border: none;
   color: white;
   outline: 0;
   background-color: transparent;
-  border-bottom: 2px solid #d62222;
+  border-bottom: 2px solid tan;
 `;
 
-export const StyledButton = styled(StyledInput)`
+export const StyledInput = styled('input')`
+  ${StyleForInput}
+`;
+
+export const StyledButton = styled('button')`
+  ${StyleForInput}
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #d62222;
   border: none;
   cursor: pointer;
+
+  &:disabled {
+    background-color: rgba(128, 128, 128, 0.1);
+    pointer-events: none;
+  }
 `;
 
 const StyledContactsForm = styled('form')`
@@ -31,36 +46,79 @@ const StyledContactsForm = styled('form')`
 `;
 
 type Inputs = {
-  name: string;
-  email: string;
-  text: string;
+  from_name: string;
+  from_email: string;
+  message: string;
 };
 
 function ContactsForm() {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const { register, handleSubmit, reset } = useForm<Inputs>();
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const [status, setStatus] = useState<null | 'sent' | 'error'>(null);
+  const [isSending, setIsSending] = useState<boolean>(false);
+
+  const form = useRef<HTMLFormElement | null>(null);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+    if (status !== null) {
+      timerId = setTimeout(() => {
+        setStatus(null);
+      }, 3000);
+    }
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [status]);
+
+  const onSubmit: SubmitHandler<Inputs> = () => {
+    setIsSending(true);
+    emailjs
+      .sendForm(
+        emailServiceId,
+        emailTemplateId,
+        form.current as HTMLFormElement,
+        {
+          publicKey: emailPublickKey,
+        }
+      )
+      .then(() => {
+        reset();
+        setStatus('sent');
+      })
+      .catch((error) => {
+        console.error(error);
+        setStatus('error');
+      })
+      .finally(() => {
+        setIsSending(false);
+      });
+  };
 
   return (
-    <StyledContactsForm onSubmit={handleSubmit(onSubmit)}>
+    <StyledContactsForm onSubmit={handleSubmit(onSubmit)} ref={form}>
       <h2>Contact me</h2>
       <StyledInput
         type="name"
-        {...register('name', { required: true })}
+        {...register('from_name', { required: true })}
         placeholder="Enter name"
       />
       <StyledInput
         type="email"
-        {...register('email', { required: true })}
+        {...register('from_email', { required: true })}
         placeholder="Enter your email"
       />
       <StyledInput
         type="text"
-        {...register('text', { required: true })}
+        {...register('message', { required: true })}
         placeholder="Enter the text you want to send"
       />
-
-      <StyledButton type="submit" value="Send" />
+      <StyledButton type="submit" disabled={status === 'sent'}>
+        {isSending && 'Message in progress'}
+        {status === 'sent' && 'Message sent successfully'}
+        {status === 'error' && 'An error has occurred. Try again, please'}
+        {status === null && !isSending && 'Send'}
+      </StyledButton>
     </StyledContactsForm>
   );
 }
