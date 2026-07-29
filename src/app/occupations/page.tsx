4f847b720b7fc549f01f800f1350cb2caa4fac7f
KseniiaSavitskaia <kseniia.savitskaia@gmail.com>
2026-07-29 18:55:29 +0300

    feat: update Occupation

--- src/app/occupations/page.tsx
+++ src/app/occupations/page.tsx
@@ -1,181 +1,181 @@
 "use client";
 
-import React from "react";
-import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
 import { AcademicCapIcon, BriefcaseIcon } from "@heroicons/react/24/outline";
-
-export const StyledMainContent = styled("div")`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 1rem;
-    gap: 1rem;
-    position: relative;
-    overflow: auto;
-    width: 100%;
-`;
-
-const StyledOccupation = styled(StyledMainContent)`
-    .occupation__title {
-        display: flex;
-        gap: 1rem;
-        align-items: center;
-    }
-
-    .occupation__chapter {
-        display: flex;
-        flex-direction: column;
-        gap: 0.5rem;
-    }
-`;
-
-const StyledOccupationItem = styled("li")`
-    background-color: rgba(128, 128, 128, 0.1);
-    padding: 1rem;
-
-    .occupation-item_title-container {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        gap: 1rem;
-    }
-
-    .occupation-item_features {
-        margin-left: 1rem;
-    }
-`;
+import styles from "./occupations.module.scss";
+import Icon from "@/components/Icon/Icon";
 
 function Occupation() {
     return (
-        <StyledOccupation>
-            <h1>Occupation</h1>
+        <>
+            <h2 className={styles["occupation__chapter-title"]}>
+                <Icon icon={BriefcaseIcon} />
+                Related work experience
+            </h2>
+            <ul className={styles.occupation__chapter}>
+                <li className={styles.occupation__item}>
+                    <Link href="https://creos.me/" target="_blank" className={styles.occupation__summary}>
+                        <h3>CREOS Play / Fronted-developer</h3>
+                        <span>07/2024-07/2026</span>
+                    </Link>
+                    <ul className={styles.occupation__list}>
+                        <li>
+                            <Link href="https://creos.me/" target="_blank">
+                                creos.me
+                            </Link>{" "}
+                            — Developed the company&apos;s corporate website
+                        </li>
+                        <li>
+                            <Link href="https://market.creosplay.io/" target="_blank">
+                                market.creosplay.io
+                            </Link>{" "}
+                            — Developed the frontend for a landing page promoting one of the company&apos;s services
+                        </li>
+                        <li>
+                            <Link href="https://creosplay.io/" target="_blank">
+                                creosplay.io
+                            </Link>{" "}
+                            — Contributed to the development of a video creative production management platform. The
+                            platform enables clients to create and manage orders, review deliverables, and provide
+                            feedback. Implemented a role-based system for clients, designers, and managers. Clients can
+                            create orders and generate AI-assisted creative concepts and previews (images and videos).
+                            Designers manage assigned tasks, track progress, and handle financial workflows. Managers
+                            assign work, monitor project progress, manage team workloads, and administer the platform
+                        </li>
+                        <li>
+                            Developed an internal analytics portal for visualizing the company&apos;s key business
+                            metrics. The system provides dashboards for subcontractor performance, financial
+                            profitability metrics, and project statistics. Implemented analytics for designers,
+                            including workload, revenue, completed projects, and individual performance indicators,
+                            enabling data-driven monitoring and operational decision-making.
+                        </li>
+                    </ul>
+                </li>
+                <li className={styles.occupation__item}>
+                    <Link href="https://websovet.ru" target="_blank" className={styles.occupation__summary}>
+                        <h3>Websovet / Fronted-developer</h3>
+                        <span>04/2024-06/2024</span>
+                    </Link>
+                    <ul className={styles.occupation__list}>
+                        <li>
+                            <Link href="https://websovet.ru/" target="_blank">
+                                websovet.ru
+                            </Link>{" "}
+                            - Developed the company&apos;s corporate website
+                        </li>
+                        <li>Creating concepts for match-3 games</li>
+                    </ul>
+                </li>
+            </ul>
 
-            <h2 className="occupation__title">
-                <AcademicCapIcon className="styled-svg styled-svg_big styled-svg_red" />
+            <h2 className={styles["occupation__chapter-title"]}>
+                <Icon icon={AcademicCapIcon} />
                 Education
             </h2>
-
-            <ul className="occupation__chapter">
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <Link href="https://en.altstu.ru/" target="_blank">
-                            <h3>Altai State Technical University</h3>
-                        </Link>
-                        <h4>2011-2016</h4>
-                    </div>
-
-                    <h5>Bachelor of Architecture</h5>
-                    <p>Design of buildings, structures and small architectural forms. Interior design.</p>
-                </StyledOccupationItem>
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <Link href="https://rs.school/" target="_blank">
-                            <h3>The Rolling Scopes School</h3>
-                        </Link>
-                        <h4>2021-2022</h4>
-                    </div>
-
-                    <ul className="occupation-item_features">
+            <ul className={styles.occupation__chapter}>
+                <li className={styles.occupation__item}>
+                    <Link href="https://en.altstu.ru/" target="_blank" className={styles.occupation__summary}>
+                        <h3>Altai State Technical University / Architecture</h3>
+                        <span>2011-2016</span>
+                    </Link>
+                    <ul className={styles.occupation__list}>
+                        <li>Design of buildings, interior, structures and small architectural forms</li>
+                        <li>Visual Design Principles</li>
+                        <li>CAD for engineering</li>
+                    </ul>
+                </li>
+                <li className={styles.occupation__item}>
+                    <Link href="https://rs.school/" target="_blank" className={styles.occupation__summary}>
+                        <h3>The Rolling Scopes School</h3>
+                        <span>2021-2022</span>
+                    </Link>
+                    <ul className={styles.occupation__list}>
                         <li>
                             <Link href="https://app.rs.school/certificate/kk9vy6aa" target="_blank">
-                                <h5>JavaScript course</h5>
+                                JavaScript course
                             </Link>
                         </li>
                         <li>
                             <Link href="https://app.rs.school/certificate/u3kin2dj" target="_blank">
-                                <h5>React course</h5>
+                                React course
                             </Link>
                         </li>
                     </ul>
-                </StyledOccupationItem>
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <Link href="/" target="_blank">
-                            <h3>FreeCodeCamp</h3>
-                        </Link>
-
-                        <h4>2020-2023</h4>
-                    </div>
-                    <ul className="occupation-item_features">
-                        <li>
-                            <h5>Responsive Web Design</h5>
-                        </li>
-                        <li>
-                            <h5>JavaScript Algorithms and Data Structures</h5>
-                        </li>
-                        <li>
-                            <h5>Front End Development Libraries</h5>
-                        </li>
-                        <li>
-                            <h5>Back End Development and APIs</h5>
-                        </li>
+                </li>
+                <li className={styles.occupation__item}>
+                    <Link href="/" target="_blank" className={styles.occupation__summary}>
+                        <h3>FreeCodeCamp</h3>
+                        <span>2020-2023</span>
+                    </Link>
+                    <ul className={styles.occupation__list}>
+                        <li>Responsive Web Design</li>
+                        <li>JavaScript Algorithms and Data Structures</li>
+                        <li>Front End Development Libraries</li>
+                        <li>Back End Development and APIs</li>
                     </ul>
-                </StyledOccupationItem>
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <h3>
-                            <Link href="https://www.codewars.com/users/savitskaya.kseniya.22" target="_blank">
-                                Codewars
-                            </Link>
-                        </h3>
-                        <h4>2020-2024</h4>
-                    </div>
-
+                </li>
+                <li className={styles.occupation__item}>
+                    <Link
+                        href="https://www.codewars.com/users/savitskaya.kseniya.22"
+                        target="_blank"
+                        className={styles.occupation__summary}>
+                        <h3>Codewars</h3>
+                        <span>2020-...</span>
+                    </Link>
                     <Image src="/icons/codewars-large.svg" width={250} height={50} alt="Codewars profile" />
-                </StyledOccupationItem>
+                </li>
             </ul>
-
-            <h2 className="occupation__title">
-                <BriefcaseIcon className="styled-svg styled-svg_big styled-svg_red" />
+            <h2 className={styles["occupation__chapter-title"]}>
+                <Icon icon={BriefcaseIcon} />
                 Unrelated work experience
             </h2>
-
-            <ul className="occupation__chapter">
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <h3>Google</h3>
-                        <h4>2013</h4>
+            <ul className={styles.occupation__chapter}>
+                <li className={styles.occupation__item}>
+                    <div className={styles.occupation__summary}>
+                        <h3>Yandex / Asessor</h3>
+                        <span>2016-2022</span>
                     </div>
-
-                    <h5>Field agent</h5>
-                    <p>
-                        A short-term project on collecting, processing, and analyzing information about local
-                        organizations and enterprises
-                    </p>
-                </StyledOccupationItem>
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <Link href="https://quickpay.ru/" target="_blank">
-                            <h3>Quickpay - payments system</h3>
-                        </Link>
-
-                        <h4>2013-2016</h4>
+                    <ul className={styles.occupation__list}>
+                        <li>Ranking the results of search queries by relevance</li>
+                        <li>Analysis of text data and media content</li>
+                    </ul>
+                </li>
+                <li className={styles.occupation__item}>
+                    <div className={styles.occupation__summary}>
+                        <h3>Mail.ru / Asessor</h3>
+                        <span>2018</span>
                     </div>
+                    <ul className={styles.occupation__list}>
+                        <li>Ranking the results of search queries by relevance</li>
+                        <li>Analysis of text data and media content</li>
+                    </ul>
+                </li>
+                <li className={styles.occupation__item}>
+                    <Link href="https://quickpay.ru/" target="_blank" className={styles.occupation__summary}>
+                        <h3>Quickpay - payments system / Client support</h3>
+                        <span>2013-2016</span>
+                    </Link>
 
-                    <h5>Client support</h5>
-                    <p>Administration and control of the payment process.</p>
-                </StyledOccupationItem>
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <h3>Yandex</h3>
-                        <h4>2016-2022</h4>
+                    <ul className={styles.occupation__list}>
+                        <li>Administration and control of the payment process</li>
+                        <li>Problem solving</li>
+                    </ul>
+                </li>
+                <li className={styles.occupation__item}>
+                    <div className={styles.occupation__summary}>
+                        <h3>Google / Field agent</h3>
+                        <span>2013</span>
                     </div>
-                    <h5>Asessor</h5>
-                    <p>Ranking the results of search queries by relevance. Analysis of text data and media content.</p>
-                </StyledOccupationItem>
-                <StyledOccupationItem>
-                    <div className="occupation-item_title-container">
-                        <h3>Mail.ru</h3>
-                        <h4>2018</h4>
-                    </div>
-
-                    <h5>Asessor</h5>
-                    <p>Ranking the results of search queries by relevance. Analysis of text data and media content.</p>
-                </StyledOccupationItem>
+                    <ul className={styles.occupation__list}>
+                        <li>
+                            A short-term project on collecting, processing, and analyzing information about local
+                            organizations and enterprises
+                        </li>
+                    </ul>
+                </li>
             </ul>
-        </StyledOccupation>
+        </>
     );
 }
 
